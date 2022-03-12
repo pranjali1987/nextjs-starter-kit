9eaf4dbd571072f03d52e244180efea31890227a
pranjali1987 <agrawal.pranjali@gmail.com>
2022-03-12 12:59:27 +0530

    added a starter kit for nextjs

---
+++ tests/index.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen } from "../utils/custom-test";
+import HomePage from "../pages/index";
+
+describe("HomePage", () => {
+  it("should render the heading", () => {
+    render(<HomePage />);
+
+    const heading = screen.getByText(/Home/i);
+
+    expect(heading).toBeInTheDocument();
+  });
+});
